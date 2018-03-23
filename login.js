88ca0849ea355477d0e79b3f99e1131ab9cf3c85
Shailaja Kant Singh <shailaja.enbake@gmail.com>
2018-03-23 18:49:26 +0530

    Jquery plugin added and used

--- login.js
+++ login.js
@@ -1,50 +1,42 @@
-$(document).ready(function()
-{
+	$(document).ready(function(){
+		$.validator.addMethod("lowercase",function(value, element)
+		{
+			return this.optional(element)||/[a-z]+/.test(value)},""),
+		$.validator.addMethod("upercase",function(value, element)
+		{
+			return this.optional(element)||/[A-Z]+/.test(value)},""),
+		$.validator.addMethod("specialchar",function(value, element)
+		{
+			return this.optional(element)||/^[a-z0-9\_]+$/i.test(value)},""),
+		$("#form").validate(
+		{
+			rules:{
+				email:{
+					required:!0
+				},
+				password:{
+					required:!0,minlength:8,
+					lowercase:!0,
+					upercase:!0,
+					specialchar:!0
+				}
+			},
+			messages:{
+				password:{
+					required:"Enter password",
+					minlength:"Length should be greater than 8",
+					lowercase:"One Lower case Letter",
+					upercase:" One Upper case Letter",
+					specialchar:"no special char"}
 
-    $('._login').click(function()
-     {
-        var sEmail = $('._email').val();
-        var sPassword = $('._password').val();
-        if ($.trim(sEmail).length == 0)
-         {
-            $('._login').parents().find('._emailError').removeClass('hide');
-            return false;
-         }
+				},
 
-        if (sEmail) 
-        {
-            var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-            if (filter.test(sEmail)) 
-            {
-                $('._login').parents().find('._emailError').addClass('hide');
-             }
-
-         else
-         {
-           $('._login').parents().find('._emailError').removeClass('hide');
-                return false;
-          }
-
-        }
-
-        if ($.trim(sPassword).length == 0)
-         {
-            $('._login').parents().find('._passError').removeClass('hide');
-            return false;
-         }
-        if (sPassword)
-          {
-              var filterpass = /^(?=.*[a-z])(?=.*[A-Z]).{7,}$/;
-            if (filterpass.test(sPassword))
-             {
-                $('._login').parents().find('._passError').addClass('hide');
-             }
-            else {
-                   $('._login').parents().find('._passError').removeClass('hide');
-                    return false;
-                  }
-           }      
-    });
-});
-
-
+				highlight: function (element) {
+					$(element).parent().addClass('error')
+				},
+				unhighlight: function (element) {
+					$(element).parent().removeClass('error')
+				}
+			}
+			) 
+	});
